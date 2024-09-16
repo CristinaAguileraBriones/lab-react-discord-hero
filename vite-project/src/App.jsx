12a335aee8d9072a38806681fdf36ab1ce054e9f
Cristina Aguilera <developer.c.93.c@gmail.com>
2024-09-16 23:29:35 +0200

    lab done

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <div className="App">
+        <nav className="navegador">            
+          <img src="../assets/discord-logo-withe.png" alt="logo-discord" />
+          <img src="../assets/menu-icon.png" alt="menu"/>
+        </nav>
+        <h1 style = {{fontFamily: 'Poppins'}}>Imagine a place...</h1>
+        <p style = {{fontFamily: 'Roboto'}}>...where you can belong to a school club, a gaming group or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes ir easy to talk every day and hang out more often</p>
+        <button>Download for Mac</button>
+        <br></br>
+        <br></br>
+        <button>Open Discord in your browser</button>
+        </div>
+    </>
+  )
+}
+
+export default App
